468c1f80146ffdc853199eb98adcc13b3355c2bb
Alfaj Mahabri <84325001+alfajmahabri@users.noreply.github.com>
2025-05-08 21:53:43 +0530

    Create external.js

---
+++ Js/external.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Internal JS Example</title>
+  <script>
+    function showAlert() {
+      alert("Hello from Internal JavaScript!");
+    }
+  </script>
+</head>
+<body>
+  <h1>Click the button</h1>
+  <button onclick="showAlert()">Click Me</button>
+</body>
+</html>
